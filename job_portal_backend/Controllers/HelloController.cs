f5af7eb9b4d0af4ffae66e13bad1264b85fc04fe
shreyask2 <prasad.a1405@gmail.com>
2026-05-19 16:16:43 +0530

    Initial backend setup

---
+++ job_portal_backend/Controllers/HelloController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace job_portal_backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                message = "Hello API Working Successfully"
+            });
+        }
+    }
+}
