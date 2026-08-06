e16e0ec2ea1b2fdfa69082f84a8cf22d6a124913
shreyask2 <prasad.a1405@gmail.com>
2026-08-06 17:14:30 +0530

    chnages

---
+++ JobPortal.Services/IImplement/IAdmin/IAuditLogService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPortal.Services.IImplement.IAdmin
+{
+    internal interface IAuditLogService
+    {
+    }
+}
---
+++ JobPortal.Services/Implement/Admin/AuditLogService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPortal.Services.Implement.Admin
+{
+    internal class AuditLogService
+    {
+    }
+}
